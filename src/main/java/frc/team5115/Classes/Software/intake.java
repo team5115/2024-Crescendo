25946e9156c4d0d887048b4051d2d5039a69dcae
avidcoder27 <gilireeves@gmail.com>
2024-01-10 17:14:17 -0500

    renamed intake.java > Intake.java

--- src/main/java/frc/team5115/Classes/Software/intake.java
+++ src/main/java/frc/team5115/Classes/Software/intake.java
@@ -3,10 +3,10 @@ package frc.team5115.Classes.Software;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team5115.Classes.Hardware.HardwareIntake;
 
-public class intake extends SubsystemBase {
+public class Intake extends SubsystemBase {
     HardwareIntake HardwareIntake;
     
-    public intake(HardwareIntake hardwareIntake){
+    public Intake(HardwareIntake hardwareIntake){
         this.HardwareIntake = hardwareIntake;
     }
 
