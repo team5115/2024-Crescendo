7b1f777f3b2ad0eaec2cc6c2488f3978095c566c
avidcoder27 <gilireeves@gmail.com>
2024-01-11 17:02:16 -0500

    joystick buttons

--- src/main/java/frc/team5115/Robot/RobotContainer.java
+++ src/main/java/frc/team5115/Robot/RobotContainer.java
@@ -2,15 +2,24 @@ package frc.team5115.Robot;
 
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.team5115.Classes.Accessory.I2CHandler;
 import frc.team5115.Classes.Hardware.HardwareArm;
 import frc.team5115.Classes.Hardware.HardwareDrivetrain;
+import frc.team5115.Classes.Hardware.HardwareFeeder;
+import frc.team5115.Classes.Hardware.HardwareIntake;
+import frc.team5115.Classes.Hardware.HardwareShooter;
 import frc.team5115.Classes.Hardware.NAVx;
 import frc.team5115.Classes.Software.Arm;
 import frc.team5115.Classes.Software.Drivetrain;
+import frc.team5115.Classes.Software.Feeder;
+import frc.team5115.Classes.Software.Intake;
 import frc.team5115.Classes.Software.PhotonVision;
+import frc.team5115.Classes.Software.Shooter;
 import frc.team5115.Commands.Auto.AutoCommandGroup;
 
 public class RobotContainer {
@@ -22,6 +31,9 @@ public class RobotContainer {
     private final I2CHandler i2cHandler;
     private final NAVx navx;
     private final Arm arm;
+    private final Feeder feeder;
+    private final Intake intake;
+    private final Shooter shooter;
     private AutoCommandGroup autoCommandGroup;
 
     public RobotContainer() {
@@ -41,10 +53,26 @@ public class RobotContainer {
         HardwareArm hardwareArm = new HardwareArm(navx, i2cHandler);
         arm = new Arm(hardwareArm);
 
+        HardwareFeeder hardwareFeeder = new HardwareFeeder();
+        HardwareIntake hardwareIntake = new HardwareIntake();
+        HardwareShooter hardwareShooter = new HardwareShooter();
+        feeder = new Feeder(hardwareFeeder);
+        intake = new Intake(hardwareIntake);
+        shooter = new Shooter(hardwareShooter);
+
         configureButtonBindings();
     }
 
-    public void configureButtonBindings() {      
+    public void configureButtonBindings() {     
+        new JoystickButton(joyManips, XboxController.Button.kA.value)
+            .onTrue(new InstantCommand(shooter :: startShooter))
+            .onFalse(new InstantCommand(shooter :: stopShooter));
+        new JoystickButton(joyManips, XboxController.Button.kB.value)
+            .onTrue(new InstantCommand(intake :: startIntakeMotor))
+            .onFalse(new InstantCommand(intake :: stopIntakeMotor));
+        new JoystickButton(joyManips, XboxController.Button.kX.value)
+            .onTrue(new InstantCommand(feeder :: startFeed))
+            .onFalse(new InstantCommand(feeder :: stopFeed));
     }
 
     public void disabledInit(){
