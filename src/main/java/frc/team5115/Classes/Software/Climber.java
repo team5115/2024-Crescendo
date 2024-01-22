f0b0100e7b0315b0c47869e6d3eb7f18d59115b0
AvidCoder <gilireeves@gmail.com>
2024-01-22 16:00:51 -0500

    climber fixed

--- src/main/java/frc/team5115/Classes/Software/Climber.java
+++ src/main/java/frc/team5115/Classes/Software/Climber.java
@@ -1,13 +1,26 @@
 package frc.team5115.Classes.Software;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team5115.Classes.Hardware.HardwareClimber;
+import frc.team5115.Classes.Hardware.NAVx;
 
 public class Climber extends SubsystemBase {
     HardwareClimber leftClimber;
     HardwareClimber rightClimber;
+    PIDController pidController;
+    double angle;
     public Climber(){
         leftClimber = new HardwareClimber(0, 1);
-        rightClimber = new HardwareClimber(0, 2);
+        leftClimber = new HardwareClimber(0, 2);
+        pidController = new PIDController(1, 1, 1);
+
     }
+    public void MoveUp(double angle){
+        double xl = pidController.calculate(leftClimber.getAngle(), angle);
+        leftClimber.setspeed(xl);
+        double xr = pidController.calculate(leftClimber.getAngle(), angle);
+        rightClimber.setspeed(xr);
+    } 
+    
 }
