ceef81b929a5cbd0cf246ec889a739a70db0030d
Idiakosa <kosa3200@gmail.com>
2024-01-23 16:17:33 -0500

    .

--- src/main/java/frc/team5115/Classes/Software/AimAndRange.java
+++ src/main/java/frc/team5115/Classes/Software/AimAndRange.java
@@ -8,7 +8,6 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-=
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
