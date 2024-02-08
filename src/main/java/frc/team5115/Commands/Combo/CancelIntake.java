f8775e0f2cbf1d9bcaf48c306d26a0694e677bf9
Scorchify <139656419+Scorchify@users.noreply.github.com>
2024-02-08 16:29:19 -0500

    Gilli Carried..

---
+++ src/main/java/frc/team5115/Commands/Combo/CancelIntake.java
@@ -0,0 +1,23 @@
+package frc.team5115.Commands.Combo;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.team5115.Classes.Software.Arm;
+import frc.team5115.Classes.Software.Intake;
+import frc.team5115.Classes.Software.Shooter;
+import frc.team5115.Commands.Arm.StowArm;
+
+public class CancelIntake extends SequentialCommandGroup {
+    public CancelIntake(Intake intake, Shooter shooter, Arm arm, DigitalInput sensor, IntakeSequence intakeSequence, ShootSequence shootSequence) { 
+        
+        addCommands(
+            new InstantCommand(intakeSequence :: cancel ),
+            new InstantCommand(intake :: stop),
+            new InstantCommand(shooter :: stop),
+            new StowArm(arm)
+           
+);
+    }
+
+}
