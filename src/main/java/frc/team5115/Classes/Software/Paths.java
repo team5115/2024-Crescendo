2f56d4fcd0f2877d1b6a36ced9b87628b15614ed
Idiakosa <kosa3200@gmail.com>
2024-03-06 15:22:58 -0500

    stuff

--- src/main/java/frc/team5115/Classes/Software/Paths.java
+++ src/main/java/frc/team5115/Classes/Software/Paths.java
@@ -8,36 +8,12 @@ import com.pathplanner.lib.path.PathPlannerTrajectory;
 public class Paths {
     public final static PathPlannerPath SideAutoPt1 = PathPlannerPath.fromPathFile("Unbasic Test");
 
-// // from top note 
-//     public final static PathPlannerPath AutoNoteToNote1 = PathPlannerPath.fromPathFile("top to middle");
-//     public final static PathPlannerPath AutoNoteToNote2 = PathPlannerPath.fromPathFile("top to bottom");
-
-// from middle note
-    public final static PathPlannerPath AutoNoteToNote3 = PathPlannerPath.fromPathFile("middle to bottom");
-    public final static PathPlannerPath AutoNoteToNote4 = PathPlannerPath.fromPathFile("middle to top");
-
-// // from bottom note
-//     public final static PathPlannerPath AutoNoteToNote5 = PathPlannerPath.fromPathFile("bottom to top");
-//     public final static PathPlannerPath AutoNoteToNote6 = PathPlannerPath.fromPathFile("bottom to middle");
-
-
-
-// starting from middle 
-    public final static PathPlannerPath AutoStartToNote7 = PathPlannerPath.fromPathFile("START middle to middle");
-    public final static PathPlannerPath AutoStartToNote8 = PathPlannerPath.fromPathFile("START middle to bottom");
-    public final static PathPlannerPath AutoStartToNote9 = PathPlannerPath.fromPathFile("START middle to top");
-
-// starting from top
-    public final static PathPlannerPath AutoStartToNote = PathPlannerPath.fromPathFile("START top to top");
-    public final static PathPlannerPath AutoNoteToNote9 = PathPlannerPath.fromPathFile("START top to middle");
-    public final static PathPlannerPath AutoNoteToNote = PathPlannerPath.fromPathFile("START top to bottom");
-
-// starting from bottom
-    public final static PathPlannerPath AutoNoteToNote7 = PathPlannerPath.fromPathFile("START bottom to top");
-    public final static PathPlannerPath AutoNoteToNote8 = PathPlannerPath.fromPathFile("START bottom to middle");
-    public final static PathPlannerPath AutoNoteToNote0 = PathPlannerPath.fromPathFile("START bottom to bottom");
-    
-
+    public final static PathPlannerPath Part1 = PathPlannerPath.fromPathFile("Path Uno");
+    public final static PathPlannerPath Part2 = PathPlannerPath.fromPathFile("Path Dos");
+    public final static PathPlannerPath Part3 = PathPlannerPath.fromPathFile("Path Tres");
+    public final static PathPlannerPath Part4 = PathPlannerPath.fromPathFile("Path Quatro");
+    public final static PathPlannerPath Part5 = PathPlannerPath.fromPathFile("Path Cinco");
+    public final static PathPlannerPath Part6 = PathPlannerPath.fromPathFile("Path Seis");
     
 }
 
